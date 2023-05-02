bf93d23afad32ac931fd27757a074db91a0c371d
The Kinky Coder <gh@thekinkycoder.com~>
2023-05-02 10:47:15 +0100

    Cleaned up the code a smidge and added some more comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,19 @@
 #include <RCSwitch.h>
 #include <FastLED.h>
 
-// #define DEBUG           // enable debug messages to serial monitor (comment out to disable)
-#define USE_LED         // enable LED strip (comment out to disable)
+#define DEBUG             // enable debug messages to serial monitor (comment out to disable)
+#define USE_LED           // enable LED strip (comment out to disable)
 
-#define TOY_ON_MIN  1   // minimum time in minutes to turn on toy
-#define TOY_ON_MAX  3   // maximum time in minutes to turn on toy
-#define TOY_OFF_MIN 5   // minimum time in minutes to turn off toy
-#define TOY_OFF_MAX 30  // maximum time in minutes to turn off toy
+#define TOY_RF_PIN    15  // The pin used to connect the RF transmitter
+#define TOY_RF_REPEAT 3   // The number of times to repeat the RF transmission (less than 3 doesn't work reliably, 3 or more will be more reliable)
 
-#define TOY_ON "101010100101010100001010"  // RF command to turn on toy
-#define TOY_OFF "101010100101010100001100" // RF command to turn off toy
+#define TOY_ON_MIN    1   // minimum time in minutes to turn on toy
+#define TOY_ON_MAX    3   // maximum time in minutes to turn on toy
+#define TOY_OFF_MIN   5   // minimum time in minutes to turn off toy
+#define TOY_OFF_MAX   30  // maximum time in minutes to turn off toy
+
+#define TOY_ON  "101010100101010100001010"  // RF command to turn on toy
+#define TOY_OFF "101010100101010100001100"  // RF command to turn off toy
 
 #ifdef USE_LED
 #define NUM_LEDS 1
@@ -35,14 +38,13 @@ void setup()
 
   // set a time to turn on toy for the first time
   nextToyActionTime = millis() + randomMilliSeconds(TOY_OFF_MIN, TOY_OFF_MAX);
-  // // Demo code
-  // nextToyActionTime = millis() + 15*1000;
 
   // setup RF to work with toy
-  vibratorRF.enableTransmit(15);
-  vibratorRF.setProtocol(1);
-  vibratorRF.setPulseLength(250);
-  vibratorRF.setRepeatTransmit(3);
+  vibratorRF.enableTransmit(TOY_RF_PIN);
+  vibratorRF.setRepeatTransmit(TOY_RF_REPEAT);
+  // Might need to change these values depending on the toy
+  vibratorRF.setProtocol(1);        // toy in question uses protocol 1
+  vibratorRF.setPulseLength(250);   // toy in question uses a pulse length of 250
 
 #ifdef USE_LED
   // setup LED strip
@@ -74,25 +76,25 @@ void loop()
   }
 #endif
 
-  if (millis() >= lastOneSecondTick + 1000)
+  // one second tick - basically only used for the debug code, I really should wrap this with the DEBUG define but leaving it incase I want it for something else.
+  if (millis() >= lastOneSecondTick)
   {
-    // one second tick
-    lastOneSecondTick = millis();
+    lastOneSecondTick = millis() + 1000;
 #ifdef DEBUG
     Serial.println("Current Millis: " + String(millis()) + " Toy State: " + String(toyState ? "ON" : "OFF") + " nextToyActionTime: " + String(nextToyActionTime) + " Millis until nextToyActionTime: " + String(nextToyActionTime - millis()));
 #endif
   }
 
-  if (millis() > nextToyActionTime)
+  // code thats fired once nextToyActionTime is reached
+  if (millis() >= nextToyActionTime)
   {
-    toyState = !toyState;
-    if (toyState)
+    toyState = !toyState; // toggle the toy state
+    if (toyState) // Turn the toy on
     {
-      vibratorRF.send(TOY_ON);
-      nextToyActionTime = millis() + randomMilliSeconds(TOY_ON_MIN, TOY_ON_MAX);
-      // // Demo code
-      // nextToyActionTime = millis() + 10*1000;
+      vibratorRF.send(TOY_ON); // send the RF command to turn on toy
+      nextToyActionTime = millis() + randomMilliSeconds(TOY_ON_MIN, TOY_ON_MAX); // update nextToyActionTime to turn off the toy after a random amount of time between min and max
 #ifdef USE_LED
+      // set the LED to green and "show" it (show actually spits out the data to the LED)
       leds[0] = CRGB::Green;
       FastLED.show();
 #endif
@@ -100,15 +102,17 @@ void loop()
       Serial.println("ON - On until " + String(nextToyActionTime));
 #endif
     }
-    else
+    else // Turn the toy off
     {
 #ifdef USE_LED
+      // set the LED to black and "show" it - black will turn off the LED and "show" will update the LED
       leds[0] = CRGB::Black;
       FastLED.show();
 #endif
+      // to make sure the toy is turned off we will send the off command multiple times - threw 10 into the for loop because of habbit, 10 is prob overkill.
       for (int i = 0; i < 10; i++)
-        vibratorRF.send(TOY_OFF);
-      nextToyActionTime = millis() + randomMilliSeconds(TOY_OFF_MIN, TOY_OFF_MAX);
+        vibratorRF.send(TOY_OFF); // send the RF command to turn off toy
+      nextToyActionTime = millis() + randomMilliSeconds(TOY_OFF_MIN, TOY_OFF_MAX); // update nextToyActionTime to turn on the toy after a random amount of time between min and max
 #ifdef DEBUG
       Serial.println("OFF - Off until " + String(nextToyActionTime));
 #endif
